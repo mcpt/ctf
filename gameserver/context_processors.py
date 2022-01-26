ae838b80e39d886201e76bedc4401897970dd24b
Paul Lee <contact@paullee.dev>
2022-01-26 00:08:17 -0500

    remove mandatory email verification requirement

--- gameserver/context_processors.py
+++ gameserver/context_processors.py
@@ -3,8 +3,6 @@ from django.conf import settings
 
 def settings_context(request):
     return {
-        "GOOGLE_ANALYTICS_ON_ALL_VIEWS": settings.GOOGLE_ANALYTICS_ON_ALL_VIEWS,
-        "GOOGLE_ANALYTICS_TRACKING_ID": settings.GOOGLE_ANALYTICS_TRACKING_ID,
         "TOS_URL": settings.TOS_URL,
         "NAVBAR": settings.NAVBAR,
         "SCHEME": settings.SCHEME,
