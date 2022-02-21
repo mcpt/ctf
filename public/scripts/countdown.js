fc343cd8a33e354cacee6da86fa7b5489f4b9f82
Paul Lee <contact@paullee.dev>
2022-02-20 20:57:57 -0500

    fix countdown when time is singular

--- public/scripts/countdown.js
+++ public/scripts/countdown.js
@@ -4,17 +4,17 @@ function countdownTimer(dateObj, elemId, expiryAction = () => { }, expiryMsg = "
 
     if (difference >= 1000) {
         const parts = {
-            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-            minutes: Math.floor((difference / 1000 / 60) % 60),
-            seconds: Math.floor((difference / 1000) % 60),
+            day: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hour: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minute: Math.floor((difference / 1000 / 60) % 60),
+            second: Math.floor((difference / 1000) % 60),
         };
         let partSum = 0;
         remaining = Object.keys(parts).filter(part => {
             partSum += parts[part];
             return partSum > 0;
         }).map(part => {
-            return `${parts[part]} ${part}`;
+            return `${parts[part]} ${part}${parts[part] != 1 ? 's' : ''}`;
         }).join(" ");
         remaining += suffix;
     } else if (difference < 0) {
