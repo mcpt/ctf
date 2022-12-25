81d529e262e17df26ec1f6691849a3a96ad51902
nyiyui <+@nyiyui.ca>
2022-12-25 14:22:14 -0500

    remove superfluous provider

--- mCTF/settings.py
+++ mCTF/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    "providers.kyii",
     "django_bootstrap5",
     "sass_processor",
     "adminsortable2",
