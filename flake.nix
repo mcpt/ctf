5aee259ee1f6fba7e7892e785d2c7ab007df31b0
nyiyui <+@nyiyui.ca>
2023-02-24 09:21:05 -0500

    cleanup unusable nix package

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
         (self: super:
           (add-setuptools super "django-bootstrap5")
           // (add-setuptools super "django-sass-processor")
+          // (add-setuptools super "uwsgi")
         );
     };
     autologin = { ... }: { services.getty.autologinUser = "root"; };
@@ -31,101 +32,5 @@
     }).env.overrideAttrs (prev: {
       buildInputs = [ pkgs.python310 pkgs.poetry ];
     });
-    packages.default = pkgs.poetry2nix.mkPoetryApplication common // { };
-    packages.image = import ./image.nix { inherit pkgs; };
-    checks.all = nixosLib.runTest {
-      name = "all";
-      hostPkgs = pkgs;
-      nodes.default = { pkgs, ... }: {
-        imports = [ autologin self.outputs.nixosModules.${system}.default ];
-        youcopter.services.uwsgi = {
-          enable = true;
-        };
-      };
-      testScript = ''
-        start_all()
-        default.wait_for_unit('uwsgi.service')
-      '';
-    };
-    checks.fastrouter = let
-      user = "youcopter-uwsgi";
-      group = "youcopter-uwsgi";
-      worker = { pkgs, ... }: {
-        imports = [ autologin ];
-        users.groups.${user} = {};
-        users.users.${group} = {
-          isSystemUser = true;
-          description = "Youcopter uWSGI";
-          group = group;
-        };
-        services.uwsgi = {
-          enable = true;
-          plugins = [ "python3" ];
-          instance.type = "emperor";
-          instance.vassals.youcopter = {
-            type = "normal";
-            pythonPackages = lib: [ self.outputs.packages.${system}.default ];
-            subscribe-to = "main:7000:example.net";
-            master = true;
-            vacuum = true;
-
-            uid = user;
-            gid = group;
-
-            plugins = [ "python3" ];
-
-            module = "mCTF.wsgi:application";
-            env = [ "DJANGO_SETTINGS_MODULE=mCTF.settings" ];
-          };
-        };
-      };
-    in nixosLib.runTest {
-      name = "fastrouter";
-      hostPkgs = pkgs;
-      nodes.worker1 = worker;
-    };
-    nixosModules.default = { config, lib, pkgs, ... }: with lib; with types; let
-      cfg = config.youcopter.services.uwsgi;
-      user = "youcopter-uwsgi";
-      group = "youcopter-uwsgi";
-    in {
-      options.youcopter.services.uwsgi = {
-        enable = mkEnableOption "Youcopter (mCTF) barebones uwsgi: CTF platform supporting multiple parallel contests";
-        config = mkOption {
-          type = submodule {
-            options = {
-            };
-          };
-        };
-      };
-      config = mkIf cfg.enable {
-        users.groups.${user} = {};
-        users.users.${group} = {
-          isSystemUser = true;
-          description = "Youcopter uWSGI";
-          group = group;
-        };
-        services.uwsgi = {
-          enable = true;
-          plugins = [ "python3" ];
-          instance.type = "emperor";
-          instance.vassals.youcopter = {
-            type = "normal";
-            pythonPackages = lib: [ self.outputs.packages.${system}.default ];
-            http = "127.0.0.1:8080";
-            master = true;
-            vacuum = true;
-
-            uid = user;
-            gid = group;
-
-            plugins = [ "python3" ];
-
-            module = "mCTF.wsgi:application";
-            env = [ "DJANGO_SETTINGS_MODULE=mCTF.settings" ];
-          };
-        };
-      };
-    };
   });
 }
