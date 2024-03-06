099c70ef5d8d47196e1728df5adc49036799453d
Jason Cameron <jasoncameron.all@gmail.com>
2024-03-06 15:59:25 -0500

    this probably will break... I MEAN WONT BREAK

--- gameserver/views/contest.py
+++ gameserver/views/contest.py
@@ -180,7 +180,7 @@ class ContestScoreboard(SingleObjectMixin, ListView, mixin.MetaMixin):
     def get_queryset(self):
         cache_key = f"contest_{self.kwargs['slug']}_scoreboard"
         queryset = cache.get(cache_key)
-        if not queryset or request.GET.get('cache_reset', '').casefold() == "yaaaa":
+        if not queryset or self.request.GET.get('cache_reset', '').casefold() == "yaaaa":
             queryset = self.object.ranks().prefetch_related('team', 'submissions__problem')
             cache.set(cache_key, queryset, 5 * 5)  # Cache for 5 minutes (300 seconds)
         return queryset
@@ -189,7 +189,7 @@ class ContestScoreboard(SingleObjectMixin, ListView, mixin.MetaMixin):
     def _get_contest(slug):
         cache_key = f"contest_{slug}_scoreboard_contest"
         contest = cache.get(cache_key)
-        if not contest or request.GET.get('cache_reset', ''.casefold()) == "yaaaa":
+        if not contest or self.request.GET.get('cache_reset', '').casefold() == "yaaaa":
             contest = get_object_or_404(models.Contest, slug=slug)
             cache.set(cache_key, contest, 5 * 5)  # Cache for 5 minutes (300 seconds)
         return contest
