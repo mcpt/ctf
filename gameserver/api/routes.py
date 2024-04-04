2e415570eee68113f02875b6c65e5b54e26232e4
Jason Cameron <git@jasoncameron.dev>
2024-04-04 16:58:06 +0000

    addedoptional field tasks

--- gameserver/api/routes.py
+++ gameserver/api/routes.py
@@ -1,6 +1,6 @@
 from django.db.models import F
 from gameserver.models.cache import ContestScore
-from gameserver.models.contest import ContestParticipation
+from gameserver.models.contest import ContestProblem
 from ninja import NinjaAPI, Schema
 from typing import List
 
@@ -16,10 +16,12 @@ class CTFSchema(Schema):
     
 class CTFTimeSchema(Schema):
     standings: List[CTFSchema]
+    tasks: List[str]
     
 @api.get("/ctftime", response=CTFTimeSchema)
 def add(request, contest_id: int):
     standings = ContestScore.ranks(contest=contest_id).annotate(pos=F("rank"), score=F("points"), team=F("participation__team__name"))
         # .only("pos", "team", "score")
+    task_names = ContestProblem.objects.filter(contest_id=contest_id).prefetch_related("problem__name").values_list("problem__name")
     
-    return {"standings": standings}
+    return {"standings": standings, "tasks": task_names}
